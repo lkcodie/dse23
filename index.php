c45611e7e6a88390a0c636d85046279ca1bf6b58
nadunkms <nadunkms@gmail.com>
2023-09-20 10:11:08 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connect360 | Your All-In-One CRM System</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container">
+
+        <div class="nav">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+         <div class="container-fluid">
+           <a class="navbar-brand" href="http://dse23.rf.gd">Connect360</a>
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+             <div class="navbar-nav">
+               <a class="nav-link active" aria-current="page" href="#">Home</a>
+               <a class="nav-link" href="#">Product</a>
+               <a class="nav-link" href="#">Solutions</a>
+               <a class="nav-link" href="#">Pricing</a>
+             </div>
+           </div>
+           </div>
+        </nav>  
+        </div>
+
+        <div class="hero">
+            <h1>Connect360: Your CRM System for Growth</h1>
+            <p>Get a complete view of your customers and grow your business with Connect360,
+               the all-in-one CRM system that's easy to use and affordable.
+            </p>
+            <button class="btn btn-primary">Start your free trial today!</button>
+        </div>
+        </div> 
+        
+        <div class="container mt-5">
+        <div class="pros">
+        <h3>With Connect360, you can</h3>
+        <ul>
+            <li>Manage your sales pipeline and opportunities</li>
+            <li>Track all of your customer interactions in one place</li>
+            <li>Automate tasks and workflows</li>
+            <li>Generate reports and analytics</li>
+            <li>And much more!</li>
+        </ul>
+        </div>
+        </div>
+    
+
+</body>
+</html>
